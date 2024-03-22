80838637fb901fa7ec08e3a92729486b53240b3a
Happmc2525 <60919012+Happmc2525@users.noreply.github.com>
2024-03-23 06:07:47 +0900

    Update kazuate.py

--- kazuate.py
+++ kazuate.py
@@ -1,24 +1,27 @@
 import discord
 import random
+import asyncio
 
 TOKEN = 'ここにbotのトークンを入れる'
 DISCORD_SERVER_IDS = ここにサーバーのidを入れる
 
 client = discord.Bot(intents=discord.Intents.all())
 
-@client.slash_command(name="kazuate", description="数当てゲーム", guild_ids=[DISCORD_SERVER_IDS])
-async def on_message(message: discord.Message):
+@bot.slash_command(name="kazuate", description="数当てゲーム", guild_ids=[DISCORD_SERVER_IDS])
+async def on_message(
+    message: discord.Message,
+    kazu: Option(int, required=False, description="難易度を変えれるよ")
+):
     ans = random.randint(1,100)
-    max_count = 5
+    max_count = kazu or 5
     kazulist = list(range(1, 101))
     await message.respond(f"1~100の数字の中から一つ選んだよ。\nその数字を{max_count}回以内に当ててね。")
-
     def kazuatecheck(m):
         return (m.author == message.author) and (m.content in [str(n) for n in kazulist])
-
     for i in range(1,max_count+1):
         await message.channel.send(f"{i}回目、いくつかな？")
-        reply = await client.wait_for("message", check=kazuatecheck)
+        async with message.channel.typing():
+            reply = await bot.wait_for("message", check=kazuatecheck)
         if int(reply.content) == ans:
             await message.channel.send("当たり！")
             break
